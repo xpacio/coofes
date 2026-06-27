cd7322ac566fed092f9fb3cbff606be271e988c1
root <root@cedis.my.domain>
2026-06-27 09:31:29 -0600

    Revert "Pico CSS migration: nav formato Pico, main container, ~200 lineas CSS eliminadas"
    This reverts commit 40a084e0150d504aac086cb8e911358ac12a0e35.

--- views/header.php
+++ views/header.php
@@ -4,30 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DBF Manager</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <header>
-        <nav class="container">
-            <ul>
-                <li><a href="?">Inicio</a></li>
-                <?php if (obtener_usuario_actual()): ?>
-                    <li><a href="?action=upload">Subir archivo</a></li>
-                    <li><a href="?action=logs">Historial</a></li>
-                    <li><a href="?action=restaurar">Restaurar</a></li>
-                    <?php if (obtener_usuario_actual()['es_admin']): ?>
-                        <li><a href="?action=usuarios">Usuarios</a></li>
-                        <li><a href="?action=rutas">Rutas</a></li>
-                    <?php endif; ?>
-                <?php endif; ?>
-            </ul>
+        <nav>
+            <a href="?">Inicio</a>
             <?php if (obtener_usuario_actual()): ?>
-            <ul>
-                <li><a href="?action=cambioclave"><?= htmlspecialchars(obtener_usuario_actual()['nombre']) ?></a></li>
-                <li><a href="?action=logout">Cerrar sesión</a></li>
-            </ul>
+                <a href="?action=upload">Subir archivo</a>
+                <a href="?action=logs">Historial</a>
+                <a href="?action=restaurar">Restaurar</a>
+                <?php if (obtener_usuario_actual()['es_admin']): ?>
+                    <a href="?action=usuarios">Usuarios</a>
+                    <a href="?action=rutas">Rutas</a>
+                <?php endif; ?>
+                <span class="user"><?= htmlspecialchars(obtener_usuario_actual()['nombre']) ?></span>
+                <a href="?action=cambioclave">Cambiar contraseña</a>
+                <a href="?action=logout">Cerrar sesión</a>
             <?php endif; ?>
         </nav>
     </header>
-    <main class="container">
+    <main>
